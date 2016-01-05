78bd69c7f9b4c55a9eb099f1e6fcb11081c55184
Jared King <j@jaredtking.com>
2016-01-05 11:12:50 -0600

    added model driver service for infuse framework - resolves #11

---
+++ src/Services/ModelDriver.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @link http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+namespace Pulsar\Services;
+
+use Pulsar\Model;
+use Pulsar\Validator;
+
+class ModelDriver
+{
+    /**
+     * @var Pulsar\Driver\DriverInterface
+     */
+    private $driver;
+
+    public function __construct($app)
+    {
+        // make the app available to models
+        Model::inject($app);
+
+        // set up the model driver
+        $config = $app['config'];
+        $class = $config->get('models.driver');
+        $this->driver = new $class($app);
+        Model::setDriver($this->driver);
+
+        // used for password hasing
+        Validator::configure(['salt' => $config->get('app.salt')]);
+    }
+
+    public function __invoke()
+    {
+        return $this->driver;
+    }
+}
