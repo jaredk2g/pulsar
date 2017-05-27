1d0a4e9e497435e29babe72397d7f41d44445166
Jared King <j@jaredtking.com>
2017-05-27 12:18:04 -0500

    added test for error stack service

---
+++ tests/Services/ErrorStackServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @see http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+use Infuse\Application;
+use Pulsar\Services\ErrorStack;
+
+class ErrorStackServiceTest extends PHPUnit_Framework_TestCase
+{
+    public function testInvoke()
+    {
+        $app = new Application();
+        $service = new ErrorStack();
+        $errors = $service($app);
+        $this->assertInstanceOf('Pulsar\ErrorStack', $errors);
+    }
+}
