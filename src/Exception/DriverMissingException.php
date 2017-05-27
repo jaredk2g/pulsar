d7a681cbc3c63ed51f6127611d9badf384af8d90
Jared King <j@jaredtking.com>
2017-05-27 07:46:24 -0500

    added missing DriverMissingException class

---
+++ src/Exception/DriverMissingException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @link http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+namespace Pulsar\Exception;
+class DriverMissingException extends ModelException
+{
+}
