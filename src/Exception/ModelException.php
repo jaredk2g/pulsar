6464804943df738f80403d6c60e79c556088ce19
Jared King <j@jaredtking.com>
2017-05-27 07:51:33 -0500

    added missing ModelException class

---
+++ src/Exception/ModelException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @link http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+namespace Pulsar\Exception;
+use Exception;
+class ModelException extends Exception
+{
+}
