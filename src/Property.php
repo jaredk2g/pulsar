3d7f5a8294a8f45a8e884859703158d9951ea069
Jared King <j@jaredtking.com>
2017-06-16 20:11:01 -0500

    added class for property type casting

---
+++ src/Property.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @see http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+
+namespace Pulsar;
+
+/**
+ * Handles value type casting.
+ */
+class Property
+{
+    /**
+     * Casts a value to a string.
+     *
+     * @param mixed $value
+     *
+     * @return string
+     */
+    public static function to_string($value)
+    {
+        return (string) $value;
+    }
+
+    /**
+     * Casts a value to an integer.
+     *
+     * @param mixed $value
+     *
+     * @return int
+     */
+    public static function to_integer($value)
+    {
+        return (int) $value;
+    }
+
+    /**
+     * Casts a value to a float.
+     *
+     * @param mixed $value
+     *
+     * @return float
+     */
+    public static function to_float($value)
+    {
+        return (float) $value;
+    }
+
+    /**
+     * Casts a value to a number.
+     *
+     * @param $value
+     *
+     * @return number
+     */
+    public static function to_number($value)
+    {
+        return $value + 0;
+    }
+
+    /**
+     * Casts a value to a boolean.
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public static function to_boolean($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_BOOLEAN);
+    }
+
+    /**
+     * Casts a date value as a UNIX timestamp.
+     *
+     * @param mixed $value
+     *
+     * @return int
+     */
+    public static function to_date($value)
+    {
+        if (!is_numeric($value)) {
+            return strtotime($value);
+        } else {
+            return $value + 0;
+        }
+    }
+
+    /**
+     * Casts a value to an array.
+     *
+     * @param mixed $value
+     *
+     * @return array
+     */
+    public static function to_array($value)
+    {
+        // decode JSON strings into an array
+        if (is_string($value)) {
+            return json_decode($value, true);
+        }
+
+        return (array) $value;
+    }
+
+    /**
+     * Casts a value to an object.
+     *
+     * @param mixed $value
+     *
+     * @return object
+     */
+    public static function to_object($value)
+    {
+        // decode JSON strings into an object
+        if (is_string($value)) {
+            return (object) json_decode($value);
+        }
+
+        return (object) $value;
+    }
+}
---
+++ tests/PropertyTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @see http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+use Pulsar\Property;
+
+class PropertyTest extends PHPUnit_Framework_TestCase
+{
+    public function testToString()
+    {
+        $this->assertEquals('string', Property::to_string('string'));
+        $this->assertEquals('123', Property::to_string(123));
+    }
+
+    public function testToInteger()
+    {
+        $this->assertEquals(123, Property::to_integer(123));
+        $this->assertEquals(123, Property::to_integer('123'));
+    }
+
+    public function testToFloat()
+    {
+        $this->assertEquals(1.23, Property::to_float(1.23));
+        $this->assertEquals(123.0, Property::to_float('123'));
+    }
+
+    public function testToNumber()
+    {
+        $this->assertEquals(123, Property::to_number(123));
+        $this->assertEquals(123, Property::to_number('123'));
+    }
+
+    public function testToBoolean()
+    {
+        $this->assertTrue(Property::to_boolean(true));
+        $this->assertTrue(Property::to_boolean('1'));
+        $this->assertFalse(Property::to_boolean(false));
+    }
+
+    public function testToDate()
+    {
+        $this->assertEquals(123, Property::to_date(123));
+        $this->assertEquals(123, Property::to_date('123'));
+        $this->assertEquals(mktime(0, 0, 0, 8, 20, 2015), Property::to_date('Aug-20-2015'));
+    }
+
+    public function testToArray()
+    {
+        $this->assertEquals(['test' => true], Property::to_array('{"test":true}'));
+        $this->assertEquals(['test' => true], Property::to_array(['test' => true]));
+    }
+
+    public function testToObject()
+    {
+        $expected = new stdClass();
+        $expected->test = true;
+        $this->assertEquals($expected, Property::to_object('{"test":true}'));
+        $this->assertEquals($expected, Property::to_object($expected));
+    }
+}
